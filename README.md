2ad5d4ac98aa72acc416e1072f8900161e97ecb4
Hyunwook Nam <hw0knam@gmail.com>
2023-09-06 18:11:54 +0900

    init: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# SaaS for Me
+
+### 웹사이트
+
+https://saas.hw0k.me
+
+### DB
+
+- 위치: `src/constants/saas-list.json`
---
+++ src/get-saas-list.ts
@@ -0,0 +1,17 @@
+import {z} from "zod";
+import saasList from './constants/saas-list.json';
+
+const schema = z.array(z.object({
+  name: z.string(),
+  description: z.string(),
+  website: z.string().url(),
+  tags: z.array(z.string()),
+}));
+
+export function getSaasList() {
+  try {
+    return schema.parse(saasList);
+  } catch (e) {
+    return [];
+  }
+}
